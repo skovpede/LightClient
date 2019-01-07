5945d0ee84ef9dd97a46420fb3e26e87d3d2b97a
Esben Skov Pedersen <esp@skovpedeconsult.dk>
2019-01-07 19:37:29 +0100

    initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SwitchComponent } from './switch/switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwitchComponent
   ],
   imports: [
     BrowserModule
